4dcac43b7edaacdb45dfa33dd08cabdbb0f2625a
Denis <denis.beauget@etu.umontpellier.fr>
2021-04-14 13:15:05 +0200

    afficheOffre + 2 hotel + REST service ready

--- RestConsole/RestClient/Program.cs
+++ RestConsole/RestClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,196 @@ namespace RestClient
 {
     class Program
     {
+        static String GetReleases(String url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.Method = "GET";
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            var content = string.Empty;
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+
+            String resultat = content.ToString();
+            return resultat.Replace("\"", "");
+        }
+
+        [Obsolete]
         static void Main(string[] args)
         {
+            System.Net.ServicePointManager.CertificatePolicy = new TrustAllCertificatePolicy();
+            String url = "https://localhost:44388/";
+
+
+            while (true)
+            {
+
+                string choix;
+
+                do
+                {
+                    Console.WriteLine("\n ===== Bienvenue dans notre application agence, veuillez choisir un hôtel partenaire =====\n");
+                    Console.WriteLine("   1 - Hôtel PasCher");
+                    Console.WriteLine("   2 - Hôtel PlusCher");
+                    Console.WriteLine("   3 - Se déconnecter");
+                    Console.WriteLine("\n Vous voulez accéder à quel serveur d'hôtel ?");
+                    Console.Write("\n ===> Votre choix : ");
+                    choix = Console.ReadLine();
+
+                    if (!choix.Equals("1") && !choix.Equals("2") && !choix.Equals("3"))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\n Erreur : Choisir entre '1', '2' ou '3'");
+                    }
+                } while (!choix.Equals("1") && !choix.Equals("2") && !choix.Equals("3"));
+
+                if (choix.Equals("1"))
+                {
+                    string choixOption;
+
+                    while (true)
+                    {
+                       Console.Clear();
+
+                        do
+                        {
+                            Console.WriteLine("\n ===== Bienvenue dans le service web de l'hôtel PasCher =====\n");
+                            Console.WriteLine("   1 - Consulter les offres");
+                            Console.WriteLine("   2 - Effectuer une réservation");
+                            Console.WriteLine("   0 - Se déconnecter");
+
+                            Console.Write("\n ===> Votre choix : ");
+                            choixOption = Console.ReadLine();
+
+                            if (!choixOption.Equals("0") && !choixOption.Equals("1") && !choixOption.Equals("2"))
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\n Erreur : Choisir entre '0', '1' ou '2'");
+                            }
+                        } while (!choixOption.Equals("0") && !choixOption.Equals("1") && !choixOption.Equals("2"));
+
+                        if (choixOption.Equals("0"))
+                        {
+                            Console.Clear();
+                            break;
+                        }
+
+                        else if(choixOption.Equals("1"))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n ===== Service : Consultation des offres =====\n");
+                            Console.WriteLine("Veuillez entrez les informations suivantes : login de l'agence, mot de passe de l'agence, date d'arrivée et de départ et nombre de personnes");
+                            String login = Console.ReadLine();
+                            String password = Console.ReadLine();
+                            String dateD = Convert.ToString(Console.ReadLine());
+                            String dateF = Convert.ToString(Console.ReadLine());
+                            int nbPersonnes  =  Convert.ToInt32(Console.ReadLine());
+
+                            string offres = GetReleases(url + "HotelPasCher/AfficherOffreDisponible?login="+login+"&password="+password+"&dateArrive="+dateD+"&dateDepart="+dateF+"&nbPersonne="+nbPersonnes);
+                            string[] resOffres = offres.Split('$');
+
+                            if (offres == null)
+                            {
+                                Console.WriteLine("liste vide");
+                            }
+
+                            for (int i = 0; i < resOffres.Length -1; i++)
+                            {
+                                string[] offreCourant = resOffres[i].Split('=');
+                                Console.WriteLine("      - Identifiant           : " + offreCourant[0]);
+                                Console.WriteLine("      - Numéro de chambre     : " + offreCourant[1]);
+                                Console.WriteLine("      - Nombre de lit         : " + offreCourant[2]);
+                                Console.WriteLine("      - Prix de l'offre       : " + offreCourant[3] + "\n");
+                            }
+
+                            Console.Write("\n Appuyer sur une touche pour revenir en arrière...");
+                            Console.ReadKey();
+                            //AJOUTEZ SUITE OPTION HOTEL1
+                        }
+                    }
+                }
+                else if(choix.Equals("2"))
+                {
+                    string choixOption;
+
+                    while (true)
+                    {
+                        Console.Clear();
+
+                        do
+                        {
+                            Console.WriteLine("\n ===== Bienvenue dans le service web de l'hôtel PlusCher =====\n");
+                            Console.WriteLine("   1 - Consulter les offres");
+                            Console.WriteLine("   2 - Effectuer une réservation");
+                            Console.WriteLine("   0 - Se déconnecter");
+
+                            Console.Write("\n ===> Votre choix : ");
+                            choixOption = Console.ReadLine();
+
+                            if (!choixOption.Equals("0") && !choixOption.Equals("1") && !choixOption.Equals("2"))
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\n Erreur : Choisir entre '0', '1' ou '2'");
+                            }
+                        } while (!choixOption.Equals("0") && !choixOption.Equals("1") && !choixOption.Equals("2"));
+
+                        if (choixOption.Equals("0"))
+                        {
+                            Console.Clear();
+                            break;
+                        }
+
+                        else if (choixOption.Equals("1"))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n ===== Service : Consultation des offres =====\n");
+                            Console.WriteLine("Veuillez entrez les informations suivantes : login de l'agence, mot de passe de l'agence, date d'arrivée et de départ et nombre de personnes");
+                            String login = Console.ReadLine();
+                            String password = Console.ReadLine();
+                            String dateD = Convert.ToString(Console.ReadLine());
+                            String dateF = Convert.ToString(Console.ReadLine());
+                            int nbPersonnes = Convert.ToInt32(Console.ReadLine());
+
+                            string offres = GetReleases(url + "HotelPlusCher/AfficherOffreDisponible?login=" + login + "&password=" + password + "&dateArrive=" + dateD + "&dateDepart=" + dateF + "&nbPersonne=" + nbPersonnes);
+                            string[] resOffres = offres.Split('$');
+
+                            if(offres == null)
+                            {
+                                Console.WriteLine("liste vide");
+                            }
+
+                            for (int i = 0; i < resOffres.Length - 1; i++)
+                            {
+                                string[] offreCourant = resOffres[i].Split('=');
+                                Console.WriteLine("      - Identifiant           : " + offreCourant[0]);
+                                Console.WriteLine("      - Numéro de chambre     : " + offreCourant[1]);
+                                Console.WriteLine("      - Nombre de lit         : " + offreCourant[2]);
+                                Console.WriteLine("      - Prix de l'offre       : " + offreCourant[3] + "\n");
+                            }
+
+                            Console.Write("\n Appuyer sur une touche pour revenir en arrière...");
+                            Console.ReadKey();
+                            //AJOUTEZ SUITE OPTION HOTEL2
+                        }
+                    }
+
+                }
+                else if(choix.Equals("3"))
+                {
+                    Console.Clear();
+                    break;
+                }
+            }
         }
     }
 }
+
---
+++ RestServiceHotel/RESTServiceHotel/Controllers/ServiceHotel2Controller.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTServiceHotel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceHotel2Controller : ControllerBase
+    {
+        public ServiceHotel2Controller()
+        {
+
+        }
+
+        IFormatProvider culture = new CultureInfo("en-US", false);
+        Agence agenceChoisis = new Agence();
+        Agence agencePartenaire1;
+        Agence agencePartenaire2;
+        List<Offre> listTemp = new List<Offre>();
+        List<Offre> listTempGUI = new List<Offre>();
+        Hotel HotelPlusCher = null;
+
+
+        [HttpGet]
+        [Route("/HotelPlusCher/CreateAndGenerate")]
+        public void Create()
+
+        {
+
+            string path = "assets\\";
+
+            this.HotelPlusCher = new Hotel("Hilton", "130 Avenue des Richou", "Paris", "France", 2, 35);
+
+            this.agencePartenaire1 = new Agence(1, "Agence des Oliviers", "87 Route des eaux, Montpellier", (float)0.2, "loginAgence1", "admin1");
+            this.agencePartenaire2 = new Agence(2, "Agence des Roses", "187 Avenue des eaux, Anger", (float)0.1, "loginAgence2", "admin2");
+
+
+            TypeChambre chambre1 = new TypeChambre(0, 2);
+            TypeChambre chambre2 = new TypeChambre(1, 1);
+            TypeChambre chambre3 = new TypeChambre(2, 3);
+            TypeChambre chambre4 = new TypeChambre(3, 4);
+            TypeChambre chambre5 = new TypeChambre(4, 2);
+
+            DateTime deb1 = DateTime.ParseExact("03/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false));
+            DateTime fin1 = DateTime.ParseExact("10/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false));
+
+            DateTime deb2 = DateTime.ParseExact("06/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false));
+            DateTime fin2 = DateTime.ParseExact("13/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false));
+
+            Offre offreTest1 = new Offre("Hilton-1", new TypeChambre(0, 2, path + "chambre1.png"), DateTime.ParseExact("03/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("10/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 400);
+            Offre offreTest2 = new Offre("Hilton-2", new TypeChambre(1, 1, path + "chambre2.png"), DateTime.ParseExact("06/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("10/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 400);
+            Offre offreTest3 = new Offre("Hilton-3", new TypeChambre(2, 3, path + "chambre2.png"), DateTime.ParseExact("03/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("06/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 790);
+            Offre offreTest4 = new Offre("Hilton-4", new TypeChambre(3, 4, path + "chambre1.png"), DateTime.ParseExact("09/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("13/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 2000);
+
+
+            Offre offreGUI1 = new Offre("Hilton-1", new TypeChambre(0, 2, "https://lemistral.eu/wp-content/uploads/quintuple/chambre-quintuple-chambre-3-lits-768x432.jpg", ""), DateTime.ParseExact("03/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("10/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 40);
+            Offre offreGUI2 = new Offre("Hilton-2", new TypeChambre(1, 1, "https://www.usine-digitale.fr/mediatheque/3/9/8/000493893_homePageUne/hotel-c-o-q-paris.jpg", ""), DateTime.ParseExact("06/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("10/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 40);
+            Offre offreGUI3 = new Offre("Hilton-3", new TypeChambre(2, 3, "https://media-cdn.tripadvisor.com/media/photo-s/09/75/9f/d5/mariafe-inn.jpg", ""), DateTime.ParseExact("03/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("06/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 90);
+            Offre offreGUI4 = new Offre("Hilton-4", new TypeChambre(3, 4, "https://www.vendee-hotel-restaurant.com/wp-content/uploads/2014/10/IMG_9063-700x467.jpg", ""), DateTime.ParseExact("09/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), DateTime.ParseExact("13/04/2021", "dd/MM/yyyy", new CultureInfo("en-US", false)), 200);
+
+            agencePartenaire1.HotelPartenaire.Add(HotelPlusCher);
+            agencePartenaire2.HotelPartenaire.Add(HotelPlusCher);
+            HotelPlusCher.ListChambres.Add(chambre1);
+            HotelPlusCher.ListChambres.Add(chambre2);
+            HotelPlusCher.ListChambres.Add(chambre3);
+            HotelPlusCher.ListChambres.Add(chambre4);
+            HotelPlusCher.ListChambres.Add(chambre5);
+
+            listTemp.Add(offreTest1);
+            listTemp.Add(offreTest2);
+            listTemp.Add(offreTest3);
+            listTemp.Add(offreTest4);
+
+
+            offreGUI1.prixTotalOffre = Convert.ToInt32((offreGUI1.fin - offreGUI1.deb).TotalDays * (HotelPlusCher.prixNuit + (agencePartenaire1.commissionAgence * HotelPlusCher.prixNuit)));
+            offreGUI2.prixTotalOffre = Convert.ToInt32((offreGUI2.fin - offreGUI2.deb).TotalDays * (HotelPlusCher.prixNuit + (agencePartenaire1.commissionAgence * HotelPlusCher.prixNuit)));
+            offreGUI3.prixTotalOffre = Convert.ToInt32((offreGUI3.fin - offreGUI3.deb).TotalDays * (HotelPlusCher.prixNuit + (agencePartenaire1.commissionAgence * HotelPlusCher.prixNuit)));
+            offreGUI4.prixTotalOffre = Convert.ToInt32((offreGUI4.fin - offreGUI4.deb).TotalDays * (HotelPlusCher.prixNuit + (agencePartenaire1.commissionAgence * HotelPlusCher.prixNuit)));
+
+            listTempGUI.Add(offreGUI1);
+            listTempGUI.Add(offreGUI2);
+            listTempGUI.Add(offreGUI3);
+            listTempGUI.Add(offreGUI4);
+        }
+
+        // GET: api/<ServiceHotel>
+        [HttpGet]
+        [Route("/HotelPlusCher/AfficherOffreDisponible")]
+        public string AfficherOffreDisponible(string login, string password, string dateArrive, string dateDepart, int nbPersonne)
+        {
+            Create();
+            DateTime dt1 = DateTime.ParseExact(dateArrive, "dd/MM/yyyy", culture);
+            DateTime dt2 = DateTime.ParseExact(dateDepart, "dd/MM/yyyy", culture);
+            this.agenceChoisis = checkConnexion(login, password);
+            List<Offre> Listres = new List<Offre>();
+            String listRes = "";
+            if (this.agenceChoisis != null)
+            {
+                foreach (Offre i in listTemp)
+                {
+                    listRes = listRes + i.idOffre +
+                        "=" + i.numChambre.nbLits +
+                        "=" + i.numChambre.numChambre +
+                        "=" + i.prixTotalOffre +
+                        "$";
+                }
+                Listres = listTemp;
+
+            }
+            else
+            {
+                Console.WriteLine("Désoler votre identification a échoué ! ");
+            }
+
+            return listRes;
+        }
+
+        [HttpGet]
+        public Agence checkConnexion(string log, string mdp)
+        {
+            if (log.Equals("loginAgence1") && mdp.Equals("admin1"))
+            {
+                Console.WriteLine("Agence 1 bien connecté ! ");
+                this.agenceChoisis = this.agencePartenaire1;
+                return agenceChoisis;
+            }
+            else if (log.Equals("loginAgence2") && mdp.Equals("admin2"))
+            {
+                Console.WriteLine("Agence 2 bien connecté ! ");
+                this.agenceChoisis = this.agencePartenaire2;
+                return agenceChoisis;
+            }
+            else
+            {
+                Console.WriteLine("Echec connexion ! ");
+                this.agenceChoisis = null;
+                return null;
+            }
+        }
+        [HttpGet]
+        [Route("/HotelPlusCher/Reservation")]
+        public string faireReservation(string login, string password, string idOffre, string nomPersonne, string prenom, int numeroCB, int nbPersonne)
+        {
+
+            if (this.HotelPlusCher == null)
+            {
+                Create();
+            }
+            checkConnexion(login, password);
+            Reservation resFinal = new Reservation();
+            String reserv = "";
+            foreach (Offre x in listTemp)
+            {
+
+
+                if (x.idOffre == idOffre)
+                {
+
+                    Reservation res = new Reservation(nomPersonne, prenom, numeroCB, x.deb, x.fin, nbPersonne, x.prixTotalOffre);
+                    resFinal = res;
+                    Client temp = new Client(nomPersonne, prenom, numeroCB);
+                    agenceChoisis.ClientAgence.Add(temp);
+                    //recherche la premiere chambre libre et fais la reservation 
+                    //si elle n'existe pas/la reservation n'a pas pu etre effectuer renvoie null
+                    TypeChambre chambre = HotelPlusCher.Reserver(res);
+                    HotelPlusCher.ListChambres.Add(chambre);
+                    reserv = reserv + res.nomClient +
+                        "=" + res.prenomClient +
+                        "=" + res.prixTotal +
+                        "=" + res.numCarteBancaire;
+                }
+            }
+            return reserv;
+        }
+
+    /*   // GET api/<ServiceHotel>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ServiceHotel>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ServiceHotel>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ServiceHotel>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
